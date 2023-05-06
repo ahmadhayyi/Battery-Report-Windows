3480debf0a1eec1c3e9f5cf1ea09ef4438ce3509
ahmadhayyi <ahmadhayyi33@gmail.com>
2023-05-06 12:44:00 +0700

    Battery Report Windows

---
+++ Battery Report.bat
@@ -0,0 +1,4 @@
+@echo off
+cd %CD%
+powercfg /batteryreport
+batteryreport.html
---
+++ script.js
@@ -0,0 +1,5 @@
+const table = document.body.querySelectorAll('table');
+const tr = table[1].querySelectorAll('tr');
+const full = tr[7].querySelectorAll('td')[1];
+console.log(full.textContent);
+
